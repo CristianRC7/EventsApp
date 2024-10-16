47b021e28fe840054db9dc0f21db6dd21d25e992
Cristian Ramirez <cristian25ramirezrc@gmail.com>
2024-10-15 21:19:27 -0400

    instalacion de librerias y conexion a la base de datos

---
+++ backend/conexion.php
@@ -0,0 +1,13 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "eventsappdb";
+
+    $conexion = new mysqli($host, $user, $pass, $db);
+
+    if($conexion->connect_error){
+        die("Error en la conexión: " . $conexion->connect_error);
+    }
+        
+?>
